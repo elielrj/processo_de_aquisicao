ca46f388450518efea51e3c59f1a57250413391d
Eliel de Souza Faria <elielrj@gmail.com>
2023-05-01 14:43:43 -0300

    modificação em dados da session do usuário

--- application/controllers/UsuarioController.php
+++ application/controllers/UsuarioController.php
@@ -1,192 +1,176 @@
 <?php
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
+class UsuarioController extends CI_Controller {
 
-class UsuarioController extends CI_Controller
-{
-	private $usuarioDAO;
+    private $usuarioDAO;
 
-	public function __construct(){
-		parent::__construct();
-	}
+    public function __construct() {
+        parent::__construct();
+    }
 
-	public function index()
-	{
-		if (!isset($this->session->email)) {
+    public function index() {
+        if (!isset($this->session->email)) {
 
-			$this->load->view('login.php');
+            $this->load->view('login.php');
+        } else {
+            $this->listar();
+        }
+    }
 
-		} else {
-			$this->listar();
-		}
-	}
+    public function listar($indice = 1) {
+        $indice--;
 
-	public function listar($indice = 1)
-	{
-		$indice--;
+        $mostrar = 10;
+        $indiceInicial = $indice * $mostrar;
 
-		$mostrar = 10;
-		$indiceInicial = $indice * $mostrar;
+        $usuarios = $this->UsuarioDAO->retrive($indiceInicial, $mostrar);
 
-		$usuarios = $this->UsuarioDAO->retrive($indiceInicial, $mostrar);
+        $quantidade = $this->UsuarioDAO->count_rows();
 
-		$quantidade = $this->UsuarioDAO->count_rows();
+        $botoes = empty($usuarios) ? '' : $this->botao->paginar('usuario/listar', $indice, $quantidade, $mostrar);
 
-		$botoes = empty($usuarios) ? '' : $this->botao->paginar('usuario/listar', $indice, $quantidade, $mostrar);
+        $dados = array(
+            'titulo' => 'Lista de Usuários',
+            'tabela' => $this->tabela->usuario($usuarios, $indiceInicial),
+            'pagina' => 'usuario/index.php',
+            'botoes' => $botoes,
+        );
 
-		$dados = array(
-			'titulo' => 'Lista de Usuários',
-			'tabela' => $this->tabela->usuario($usuarios, $indiceInicial),
-			'pagina' => 'usuario/index.php',
-			'botoes' => $botoes,
-		);
+        $this->load->view('index', $dados);
+    }
 
-		$this->load->view('index', $dados);
-	}
+    public function novo() {
 
-	public function novo()
-	{
+        $dados = array(
+            'titulo' => 'Novo Usuário',
+            'pagina' => 'usuario/novo.php',
+            'departamentos' => $this->DepartamentoDAO->options(),
+        );
 
-		$dados = array(
-			'titulo' => 'Novo Usuário',
-			'pagina' => 'usuario/novo.php',
-			'departamentos' => $this->DepartamentoDAO->options(),
-		);
+        $this->load->view('index', $dados);
+    }
 
-		$this->load->view('index', $dados);
-	}
+    public function criar() {
 
-	public function criar()
-	{
+        $data = $this->input->post();
 
-		$data = $this->input->post();
+        $usuario = new Usuario(
+                null,
+                $data['email'],
+                $data['cpf'],
+                md5($data['senha']),
+                $this->DepartamentoDAO->retriveId($data['departamento_id']),
+                $data['status']
+        );
 
-		$usuario = new Usuario(
-			null,
-			$data['email'],
-			$data['cpf'],
-			md5($data['senha']),
-			$this->DepartamentoDAO->retriveId($data['departamento_id']),
-			$data['status']
-		);
+        $this->UsuarioDAO->create($usuario);
 
-		$this->UsuarioDAO->create($usuario);
+        redirect('UsuarioController');
+    }
 
-		redirect('UsuarioController');
-	}
+    public function alterar($id) {
 
-	public function alterar($id)
-	{
+        $usuario = $this->UsuarioDAO->retriveId($id);
 
-		$usuario = $this->UsuarioDAO->retriveId($id);
+        $dados = array(
+            'titulo' => 'Alterar Usuário',
+            'pagina' => 'usuario/alterar.php',
+            'usuario' => $usuario,
+            'departamentos' => $this->DepartamentoDAO->options(),
+        );
 
-		$dados = array(
-			'titulo' => 'Alterar Usuário',
-			'pagina' => 'usuario/alterar.php',
-			'usuario' => $usuario,
-			'departamentos' => $this->DepartamentoDAO->options(),
-		);
+        $this->load->view('index', $dados);
+    }
 
-		$this->load->view('index', $dados);
+    public function atualizar() {
 
-	}
+        $data = $this->input->post();
 
-	public function atualizar()
-	{
+        $usuario = new Usuario(
+                $data['id'],
+                $data['email'],
+                $data['cpf'],
+                md5($data['senha']),
+                $this->DepartamentoDAO->retriveId($data['departamento_id']),
+                $data['status']
+        );
 
-		$data = $this->input->post();
+        $this->UsuarioDAO->update($usuario);
 
-		$usuario = new Usuario(
-			$data['id'],
-			$data['email'],
-			$data['cpf'],
-			md5($data['senha']),
-			$this->DepartamentoDAO->retriveId($data['departamento_id']),
-			$data['status']
-		);
-		
-		$this->UsuarioDAO->update($usuario);
+        redirect('UsuarioController');
+    }
 
-		redirect('UsuarioController');
-	}
+    public function deletar($id) {
 
-	public function deletar($id)
-	{
+        $this->UsuarioDAO->delete($id);
 
-		$this->UsuarioDAO->delete($id);
+        redirect('UsuarioController');
+    }
 
-		redirect('UsuarioController');
-	}
+    public function logar() {
 
-	public function logar()
-	{
+        if ($this->verificarEmail()) {
 
-		if ($this->verificarEmail()) {
+            if ($this->verificarSenha()) {
 
-			if ($this->verificarSenha()) {
+                $email = $this->input->post('email');
 
-				$email = $this->input->post('email');
+                $usuario = $this->UsuarioDAO->buscarPeloEmail($email);
 
-				$usuario = $this->UsuarioDAO->retriveEmail($email);
+                $data = array(
+                    'id' => $usuario->id,
+                    'email' => $usuario->email,
+                    'cpf' => $usuario->cpf,
+                    'senha' => $usuario->senha,
+                    'departamento_id' => $usuario->departamento->id,
+                    'status' => $usuario->status,
+                );
 
-				$data = array(
-					'id' => $usuario->id,
-					'email' => $usuario->email,
-					'status' => $usuario->status,
-					'cpf' => $usuario->cpf
-				);
+                $this->session->set_userdata($data);
 
-				$this->session->set_userdata($data);
+                redirect('ProcessoController');
+            }
+        }
 
-				redirect('ProcessoController');
-			}
+        redirect(base_url());
+    }
 
-		}
+    public function verificarEmail() {
 
-		redirect(base_url());
-	}
+        $email = $this->input->post('email');
 
-	public function verificarEmail()
-	{
+        $where = array('email' => $email);
 
-		$email = $this->input->post('email');
+        $resultado = $this->UsuarioDAO->verificarEmail($where);
 
-		$where = array('email' => $email);
+        if (isset($resultado[0])) {
 
+            $this->session->set_userdata('email_valido', true);
+        } else {
+            $this->session->set_userdata('email_valido', false);
+        }
 
-		$resultado = $this->UsuarioDAO->verificarEmail($where);
+        return isset($resultado[0]);
+    }
 
+    public function verificarSenha() {
 
-		if (isset($resultado[0])) {
+        $senha = md5($this->input->post('senha'));
 
-			$this->session->set_userdata('email_valido', true);
+        $where = array('senha' => $senha);
 
-		} else {
-			$this->session->set_userdata('email_valido', false);
-		}
+        $resultado = $this->UsuarioDAO->verificarSenha($where);
 
-		return isset($resultado[0]);
+        if (isset($resultado[0])) {
 
-	}
+            $this->session->set_userdata('senha_valida', true);
+        } else {
+            $this->session->set_userdata('senha_valida', false);
+        }
 
-	public function verificarSenha()
-	{
-
-		$senha = md5($this->input->post('senha'));
-
-		$where = array('senha' => $senha);
-
-		$resultado = $this->UsuarioDAO->verificarSenha($where);
-
-		if (isset($resultado[0])) {
-
-			$this->session->set_userdata('senha_valida', true);
-
-		} else {
-			$this->session->set_userdata('senha_valida', false);
-		}
-
-		return isset($resultado[0]);
-	}
+        return isset($resultado[0]);
+    }
 
 }
