6b95daa1015594ccee37a5d0bfc874009ac11a2a
elielrj <elielrj@gmail.com>
2023-05-26 09:42:55 -0300

    correção em DB insert user

--- db/05 - SQL de Dados dos Usuario.sql
+++ db/05 - SQL de Dados dos Usuario.sql
@@ -4,7 +4,7 @@ INSERT INTO
 `usuario`(`id`,`nome`,`sobrenome`,`email`, `cpf`, `senha`, `departamento_id`,`status`,`funcao_id`,`hierarquia_id`) 
 VALUES 
 (default,'Leitor','de Processos','leitor@leitor','09856260701',md5(123),1,true,1,2),
-(default,'Escritor','de Processos','escrito@escritor','09856260701',md5(123), 2,true,1,1),
+(default,'Escritor','de Processos','escritor@escritor','09856260701',md5(123), 2,true,1,1),
 (default,'Aprovador','de Processos','aprovador@aprovador','09856260701',md5(123), 3,true,1,4),
 (default,'Admiinistrador','do Sistema','admin@admin','09856260701',md5(123), 4,true,1,3),
 (default,'Super','Usuário','root@root','09856260701',md5(123), 1,true,1,5);
