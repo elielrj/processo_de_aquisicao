c3fa901df23470e2bc4fb0b12c2fa3c02c712729
Eliel de Souza Faria <elielrj@gmail.com>
2023-05-30 20:58:51 -0300

    Update para ajustes de arquivos

--- application/controllers/ArquivoController.php
+++ application/controllers/ArquivoController.php
@@ -12,7 +12,7 @@ class ArquivoController extends CI_Controller
     {
         parent::__construct();
         $this->load->library('ArquivoLibrary');
-        
+
         $this->load->model('dao/ArquivoDAO');
         $this->load->model('dao/ProcessoDAO');
         $this->load->model('dao/ArtefatoDAO');
@@ -95,15 +95,23 @@ class ArquivoController extends CI_Controller
         $file = $_FILES['arquivo'];
 
         if (isset($file) && isset($data_post)) {
-            
-            $arquivo = $this->moverArquivo($data_post);
-            var_dump($arquivo);
+
+
+
+            try {
+
+                $arquivo = $this->moverArquivo($data_post);
+
+            } catch (Exception $e) {
+                echo 'Exceção capturada: ', $e->getMessage(), "\n";
+            }
+
             if (isset($arquivo)) {
-               
+
                 $this->ArquivoDAO->criar($arquivo);
-                
+
                 redirect('ProcessoController/exibir/' . $data_post['processo_id']);
-                
+
             } else {
                 //todo tratar erro 
                 var_dump('caiu no else');
@@ -158,15 +166,15 @@ class ArquivoController extends CI_Controller
         $file = $_FILES['arquivo'];
 
         if (isset($file) && isset($data_post)) {
-            
+
             $arquivo = $this->moverArquivo($data_post);
-            
+
             if (isset($arquivo)) {
-                
+
                 $this->ArquivoDAO->atualizar($arquivo);
-                
+
                 redirect('ProcessoController/exibir/' . $data_post['processo_id']);
-                
+
             } else {
                 //todo tratar erro                 
             }
@@ -182,12 +190,12 @@ class ArquivoController extends CI_Controller
 
     private function toObject($data_post, $path)
     {
-        
+
 
         return new Arquivo(
             isset($data_post['id']) ? $data_post['id'] : null,
             $path,
-           DataLibrary::dataHoraBr(),
+            DataLibrary::dataHoraBr(),
             $_SESSION['id'],
             $data_post['artefato_id'],
             $data_post['processo_id'],
@@ -198,7 +206,7 @@ class ArquivoController extends CI_Controller
     private function moverArquivo($data_post)
     {
         $tmp_name = $_FILES['arquivo']['tmp_name'];
-        
+
         if (empty($data_post['arquivo_path'])) {
 
             $nome_do_arquivo = $_FILES['arquivo']['name'];
@@ -213,27 +221,33 @@ class ArquivoController extends CI_Controller
 
         }
 
-        //$arquivado = move_uploaded_file($tmp_name, $path);
-        $arquivado = false;
+        
 
-        $config['upload_path'] = $path;
-        $config['allowed_types'] = 'pdf';
-        $config['max_size'] = 10000;
-
-        $this->load->library('upload', $config);
-
-        if(! $this->upload->do_upload('arquivo'))
-        {
-            $error = array('error' => $this->upload->display_errors());
-
-            $this->load->view('arquivo/upload_error', $error);
-
-            $arquivado = false;
-        }else{
-            $arquivado = true;
+        try {
+            $arquivado = move_uploaded_file($tmp_name, $path);
+        } catch (Exception $e) {
+            var_dump('Exceção capturada: '. $e->getMessage(). "\n");
         }
 
-       
+        /* $arquivado = false;
+
+         $config['upload_path'] = $path;
+         $config['allowed_types'] = 'pdf';
+         $config['max_size'] = 10000;
+
+         $this->load->library('upload', $config);
+
+         if (!$this->upload->do_upload('arquivo')) {
+             $error = array('error' => $this->upload->display_errors());
+
+             $this->load->view('arquivo/upload_error', $error);
+
+             $arquivado = false;
+         } else {
+             $arquivado = true;
+         }*/
+
+
         if ($arquivado) {
 
             return $this->toObject($data_post, $path);
