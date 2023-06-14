6d4abdee963864788cb0f74773b286b897a66426
Eliel de Souza Faria <elielrj@gmail.com>
2023-06-14 19:05:15 -0300

    Ajuste de rota em LeiTipoArtefato.

--- application/views/index/sidebar.php
+++ application/views/index/sidebar.php
@@ -3,88 +3,90 @@
 <?php
 
 $posto_ou_raduacao_e_nome =
-    (isset($_SESSION['nome']) && isset($_SESSION['hierarquia_sigla']))
-    ? ($_SESSION['hierarquia_sigla'] . " " . $_SESSION['nome'])
-    : '';
+	(isset($_SESSION['nome']) && isset($_SESSION['hierarquia_sigla']))
+		? ($_SESSION['hierarquia_sigla'] . " " . $_SESSION['nome'])
+		: '';
 
-$nivel_de_acesso =
-    isset($_SESSION['funcao_nivel_de_acesso'])
-    ? $_SESSION['funcao_nivel_de_acesso']
-    : '';
+$nivel_de_acesso = 	$_SESSION['funcao_nivel_de_acesso'] ?? '';
 
 include_once('application/models/bo/Administrar.php');
 include_once('application/models/bo/Root.php');
 
 
 $acesso_ao_banco =
-    isset($_SESSION['funcao_nivel_de_acesso'])
-    ? (
-        ($_SESSION['funcao_nivel_de_acesso'] == Administrar::NOME ||
-            $_SESSION['funcao_nivel_de_acesso'] == Root::NOME) ? true : false)
-    : false;
+	isset($_SESSION['funcao_nivel_de_acesso'])
+		? (
+	($_SESSION['funcao_nivel_de_acesso'] == Administrar::NOME ||
+		$_SESSION['funcao_nivel_de_acesso'] == Root::NOME) ? true : false)
+		: false;
 ?>
 
 
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+	<!-- Sidebar - Brand -->
+	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
 
-        <div class="sidebar-brand-text mx-3">
-            <small>
-                <?php echo $posto_ou_raduacao_e_nome ?><sup><em>
-                        <?php echo strtolower($nivel_de_acesso); ?>
-                    </em>
-            </small></sup>
-        </div>
-    </a>
+		<div class="sidebar-brand-text mx-3">
+			<small>
+				<?php echo $posto_ou_raduacao_e_nome ?><sup><em>
+						<?php echo strtolower($nivel_de_acesso); ?>
+					</em>
+			</small></sup>
+		</div>
+	</a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active center">
-        <a class="nav-link" href="index.html">
+	</br>
+	<!-- Nav Item - Dashboard -->
+
+	<li class="nav-item active center">
+
+		<div class="sidebar-card d-nome d-lg-flex p-1">
+			<p class="text-center p-0 m-1">Versão 1.0.0.0</p>
+		</div>
+
+		<a class="nav-link" href="index.html">
             <span>
                 <img src="<?php echo base_url(); ?>icones_da_pagina/favicon-32x32.png" alt="Bootstrap" width="32"
-                    height="32">
-                iSALC
+					 height="32">
+				iSALC
             </span>
-        </a>
-    </li>
+		</a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+	</li>
 
-    <!-- Heading -->
-    <div class="sidebar-heading">Processos</div>
+	<!-- Divider -->
+	<hr class="sidebar-divider">
 
-    <?php include_once('sidebar/processo.php'); ?>
+	<!-- Heading -->
+	<div class="sidebar-heading">Processos</div>
 
-
-
-    <?php $acesso_ao_banco ? include_once('sidebar/bando_de_dados.php') : ''; ?>
+	<?php include_once('sidebar/processo.php'); ?>
 
 
 
-    <!-- Heading -->
-    <div class="sidebar-heading">Configurações</div>
-
-    <?php include_once('sidebar/configuracao_usuario.php'); ?>
+	<?php $acesso_ao_banco ? include_once('sidebar/bando_de_dados.php') : ''; ?>
 
 
+	<!-- Heading -->
+	<div class="sidebar-heading">Configurações</div>
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+	<?php include_once('sidebar/configuracao_usuario.php'); ?>
 
-    <!-- Sidebar Message -->
-    <div class="sidebar-card d-none d-lg-flex">
-        <img class="sidebar-card-illustration mb-2" src=<?php echo base_url('img/sugestao.png') ?> alt="...">
-        <p class="text-center mb-2"><strong>Sugestão</strong> deixe aqui sua sugestão para que possamos melhorar!</p>
-        <a class="btn btn-success btn-sm" href="<?php echo base_url('index.php/SugestaoController/novo') ?>">Sugestão!</a>
-    </div>
+
+	<!-- Sidebar Toggler (Sidebar) -->
+	<div class="text-center d-none d-md-inline">
+		<button class="rounded-circle border-0" id="sidebarToggle"></button>
+	</div>
+
+	<!-- Sidebar Message -->
+	<div class="sidebar-card d-none d-lg-flex">
+		<img class="sidebar-card-illustration mb-2" src=<?php echo base_url('img/sugestao.png') ?> alt="...">
+		<p class="text-center mb-2"><strong>Sugestão</strong> deixe aqui sua sugestão para que possamos melhorar!</p>
+		<a class="btn btn-success btn-sm"
+		   href="<?php echo base_url('index.php/SugestaoController/novo') ?>">Sugestão!</a>
+	</div>
 
 </ul>
 
