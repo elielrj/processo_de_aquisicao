71852b137f163c7b5661809f6b0db8f3954f8f75
Eliel de Souza Faria <elielrj@gmail.com>
2023-06-19 22:30:20 -0300

    Reajuste da view com helper.

--- application/views/arquivo/alterar.php
+++ application/views/arquivo/alterar.php
@@ -1,20 +1,25 @@
 <?php
-
 defined('BASEPATH') or exit('No direct script access allowed');
-$timezone = new DateTimeZone('America/Sao_Paulo');
-$data_hora = new DateTime($arquivo->dataDoUpload, $timezone);
 
-echo "<h1>{$titulo}</h1>" . "</br>" .
- form_open_multipart('ArquivoController/atualizar', ['class' => 'form-group']) . "</br>" .
- form_input(['name' => 'id', 'type' => 'hidden', 'value' => $arquivo->id]) . "</br>" .
- form_label('Artefato') . form_dropdown('artefato_id', $artefatos, $arquivo->artefato->id, ['class' => 'form-control', 'maxlength' => 150]) . "</br>" .
- form_label('Path') . form_input(['name' => 'path', 'class' => 'form-control', 'maxlength' => 20, 'value' => $arquivo->path]) . "</br>" .
- form_label('Nome do Arquivo') . form_input(['name' => 'nome_do_arquivo', 'class' => 'form-control', 'maxlength' => 20, 'value' => $arquivo->nomeDoArquivo]) . "</br>" .
- form_label('Data de Upload') . form_input(['name' => 'data_do_upload', 'type' => 'hidden', 'class' => 'form-control', 'maxlength' => 20, 'value' => $data_hora->format('d-m-Y H:m:s')]) . "</br>" .
- form_label('Processo') . form_dropdown('processo_id', $processos, $arquivo->processo->id, ['class' => 'form-control']) . "</br>" .
- form_label('Status') . form_dropdown('status', [true => 'Ativo', false => 'Inativo'], $arquivo->status, ['class' => 'form-control']) . "</br>" .
- form_submit('enviar', 'Enviar', ['class' => 'btn btn-primary btn-lg btn-block']) . "</br>" .
- "<a href=" . base_url('index.php/ArquivoController') . " class='btn btn-danger btn-lg btn-block' >Cancelar</a>";
+view_titulo($titulo);
+
+view_form_open_multipart(ArquivoController::$arquivoController.'/atualizar');
+
+view_input('', 'id', 'id', 'hidden', $arquivo->id);
+
+view_dropdown('Artefato', 'artefato_id', $artefatos, $arquivo->artefatoId);
+
+view_input('Path', 'path', 'path', 'text', $arquivo->path);
+
+view_input('Nome do Arquivo', 'nome', 'nome', 'text', $arquivo->nome);
+
+view_dropdown('Processo', 'processo_id', $processos, $arquivo->processoId);
+
+view_dropdown_status($arquivo->status);
+
+view_form_submit_enviar();
+
+view_form_submit_cancelar('arquivo-listar');
 
 form_close();
 ?>
