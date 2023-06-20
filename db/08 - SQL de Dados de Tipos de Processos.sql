16c7f9add357740bb693546ac092c1b6c05949d5
Eliel de Souza Faria <elielrj@gmail.com>
2023-06-20 17:48:53 -0300

    Remoção de SQL e Diagrama de UC

--- db/08 - SQL de Dados de Tipos de Processos.sql
+++
@@ -1,9 +0,0 @@
-use processo_de_aquisicao;
-
-INSERT INTO 
-`tipo`(`id`,`nome`,`status`) 
-VALUES 
-(1,'Licitação',true),
-(2,'Empenho',true);
-
-
--- db/10 - SQL de Dados do Andamento.sql
+++
@@ -1,7 +0,0 @@
-use processo_de_aquisicao;
-
-INSERT INTO `andamento`(`processo_id`,`status_do_andamento`,`data_hora`) 
-VALUES 
-(1,'enviado',date(now())),
-(2,'enviado',date(now()));
-
