8714151aa90893edf32e687842709dcb9282c812
Eliel de Souza Faria <elielrj@gmail.com>
2023-06-20 22:37:11 -0300

    Tradução de Logout.

--- application/views/index.php
+++ application/views/index.php
@@ -106,11 +106,11 @@ exit();
 					<span aria-hidden="true">×</span>
 				</button>
 			</div>
-			<div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+			<div class="modal-body">Selecione "Sair" abaixo se estiver pronto para encerrar sua sessão atual.</div>
 			<div class="modal-footer">
 				<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
 				<a class="btn btn-primary"
-				   href="<?php echo base_url('index.php/LoginController/sair'); ?>">Logout</a>
+				   href="<?php echo base_url('index.php/LoginController/sair'); ?>">Sair</a>
 			</div>
 		</div>
 	</div>
