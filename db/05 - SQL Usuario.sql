14a0c8cbbc3aee2f18e17bab4d98f69e69226f2e
Eliel de Souza Faria <elielrj@gmail.com>
2023-06-24 12:12:57 -0300

    Remoção de Usuário extra.

--- db/05 - SQL Usuario.sql
+++ db/05 - SQL Usuario.sql
@@ -6,8 +6,8 @@ VALUES
 (1,'Leitor','Leitor de Processos','leitor@leitor','09856260701',md5(123),1,true,1,1),
 (2,'Demandante','Escritor de Processos','escritor@escritor','09856260701',md5(123), 2,true,1,2),
 (3,'Aprovador Fisc Adm','Aprovador de Processos','aprovadorfiscadm@aprovadorfiscadm','09856260701',md5(123), 3,true,1,3),
-(3,'Aprovador OD','Aprovador de Processos','aprovadorod@aprovadorod','09856260701',md5(123), 3,true,1,3),
-(4,'SALC','Executor de Processos','executor@executor','09856260701',md5(123), 3,true,1,4),
-(5,'Conformador','Conformador de Processos','conformador@conformador','09856260701',md5(123), 3,true,1,5),
-(6,'Admiinistrador','Admiinistrador do Sistema','administrador@administrador','09856260701',md5(123), 4,true,1,6),
-(7,'Super','Super Usuário','root@root','09856260701',md5(123), 1,true,1,7);
+(4,'Aprovador OD','Aprovador de Processos','aprovadorod@aprovadorod','09856260701',md5(123), 3,true,1,3),
+(5,'SALC','Executor de Processos','executor@executor','09856260701',md5(123), 3,true,1,4),
+(6,'Conformador','Conformador de Processos','conformador@conformador','09856260701',md5(123), 3,true,1,5),
+(7,'Admiinistrador','Admiinistrador do Sistema','administrador@administrador','09856260701',md5(123), 4,true,1,6),
+(8,'Super','Super Usuário','root@root','09856260701',md5(123), 1,true,1,7);
