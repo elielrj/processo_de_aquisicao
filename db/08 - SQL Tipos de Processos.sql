1689dc125803723238f1cb22a1e814b6dab915c2
Eliel de Souza Faria <elielrj@gmail.com>
2023-06-24 17:42:04 -0300

    Ajustes finos.

--- db/08 - SQL Tipos de Processos.sql
+++ db/08 - SQL Tipos de Processos.sql
@@ -3,7 +3,7 @@ use processo_de_aquisicao;
 INSERT INTO 
 `tipo`(`id`,`nome`,`status`) 
 VALUES 
-(1,'Licitação',true),
-(2,'Empenho',true);
+(1,'Licitação/ Dispensa/ Inex',true),
+(2,'Empenho(só NE)/ Mapa OCS',true);
 
 
