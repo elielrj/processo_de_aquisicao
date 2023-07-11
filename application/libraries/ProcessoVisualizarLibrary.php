ace59184ce119a211ab109fc13153204ad308b85
elielrj <elielrj@gmail.com>
2023-07-11 12:50:31 -0300

    Coreção de bug na exibição de processo completo.

--- application/libraries/ProcessoVisualizarLibrary.php
+++ application/libraries/ProcessoVisualizarLibrary.php
@@ -99,7 +99,7 @@ class ProcessoVisualizarLibrary
                                                 $artefato->nome . ' - Descrição do anexo: '. $arquivo->nome .
                                             "<p>
                                             <div style='height: 1080px; width: 100%;'>
-                                                <embed src='" . $arquivo->path . "' type='application/pdf' width='100%' height='100%'>
+                                                <embed src='" . base_url($arquivo->path) . "' type='application/pdf' width='100%' height='100%'>
                                             </div>
                                         ";
                     }else{
