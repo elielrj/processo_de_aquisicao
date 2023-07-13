ed08182f7adeaeee630796a997b6884b22b32023
Eliel de Souza Faria <elielrj@gmail.com>
2023-07-13 19:03:53 -0300

    Incusão de classe Data e DataHora

---
+++ application/libraries/Data.php
@@ -0,0 +1,30 @@
+<?php
+
+class Data
+{
+	const DATE_TIME_ZONE = 'America/Sao_Paulo';
+	const FORMATO_D_M_A = 'd-m-Y';
+	const FORMATO_A_M_D_MYSQL = 'Y-m-d';
+
+	private $data;
+
+	public function __construct(
+		$dateString = 'now',
+		$dateTimeZone = self::DATE_TIME_ZONE
+	)
+	{
+		$this->data = new DateTime(
+			$dateString,
+			new DateTimeZone($dateTimeZone)
+		);
+	}
+
+	public function formatoDoBrasil()
+	{
+		return ($this->data)->format(self::FORMATO_D_M_A);
+	}
+	public function formatoDoMySQL()
+	{
+		return ($this->data)->format(self::FORMATO_A_M_D_MYSQL);
+	}
+}
---
+++ application/libraries/DataHora.php
@@ -0,0 +1,30 @@
+<?php
+
+class DataHora
+{
+	const DATE_TIME_ZONE = 'America/Sao_Paulo';
+	const FORMATO_D_M_A_H_M_S =  'd-m-Y H:i:s';
+	const FORMATO_A_M_D__H_M_SP_MYSQL = 'Y-m-d H:i:sP';
+
+	private $dataHora;
+
+	public function __construct(
+		$dateString = 'now',
+		$dateTimeZone = self::DATE_TIME_ZONE
+	)
+	{
+		$this->dataHora = new DateTime(
+			$dateString,
+			new DateTimeZone($dateTimeZone)
+		);
+	}
+
+	public function formatoDoBrasil()
+	{
+		return ($this->dataHora)->format(self::FORMATO_D_M_A_H_M_S);
+	}
+	public function formatoDoMySQL()
+	{
+		return ($this->dataHora)->format(self::FORMATO_A_M_D__H_M_SP_MYSQL);
+	}
+}
