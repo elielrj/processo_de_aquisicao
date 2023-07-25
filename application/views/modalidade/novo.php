2b04cbf315e00d8719d06e9eddaa6784f80c2303
elielrj <elielrj@gmail.com>
2023-07-25 16:17:07 -0300

    Correção de "nova modalidade"

--- application/views/modalidade/novo.php
+++ application/views/modalidade/novo.php
@@ -1,32 +1,21 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
-echo "<h1>{$titulo}</h1>" .
+<h1><?php echo $titulo; ?></h1>
 
-    form_open('ModalidadeController/criar', array('class' => 'form-group')) .
+<?php echo form_open(MODALIDADE_CONTROLLER.'/criar', ['class' => 'form-group']); ?>
 
-    form_input(['name' => 'id', 'class' => 'form-control', 'type' => 'hidden']) . "</br>" .
+</br>
+<?php echo form_label('Nome'); ?>
+<?php echo form_input(['name' => 'nome', 'class' => 'form-control', 'maxlength' => 250]); ?>
 
-    form_label('Nome') . form_input(['name' => 'nome', 'class' => 'form-control', 'maxlength' => 150]) . "</br>" .
+</br>
+<?php echo form_label('Status'); ?>
+<?php echo form_dropdown('status', [true => 'Ativo', false => 'Inativo'], true, ['class' => 'form-control']); ?>
 
-    form_label('Lei') . form_input(['name' => 'lei', 'class' => 'form-control', 'maxlength' => 150]) . "</br>" .
-   
-    form_label('Artigo') . form_input(['name' => 'artigo', 'class' => 'form-control', 'maxlength' => 10]) . "</br>" .
-   
-    form_label('Inciso') . form_input(['name' => 'inciso', 'class' => 'form-control', 'maxlength' => 10]) . "</br>" .
+</br>
+<?php echo form_submit('enviar', 'Enviar', ['class' => 'btn btn-primary btn-lg btn-block']); ?>
 
-    form_label('Data da Lei') . form_input(['name' => 'dataDaLei', 'type' => 'date', 'class' => 'form-control', 'maxlength' => 150]) . "</br>" .
+<a href="<?php echo base_url('index.php/' . MODALIDADE_CONTROLLER); ?>" class='btn btn-danger btn-lg btn-block'>Cancelar</a>
 
-    form_label('Página') . form_input(['name' => 'pagina', 'class' => 'form-control', 'maxlength' => 250]) . "</br>" .
-
-    form_label('Status') . form_dropdown('status', [true => 'Ativo', false => 'Inativo'], true, ['class' => 'form-control']) . "</br>" .
-
-    form_submit('enviar', 'Enviar', ['class' => 'btn btn-primary btn-lg btn-block']) .
-    "<a href=" . base_url('index.php/ModalidadeController') . " class='btn btn-danger btn-lg btn-block' >Cancelar</a>";
-
-echo form_close();
-
-//todo refazer
-
-?>
-
+<?php echo form_close(); ?>
