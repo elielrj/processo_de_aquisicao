36de04184ab4b2b95b2ea4454f812eb23d9662c9
Eliel <elielrj@gmail.com>
2023-07-27 19:33:53 -0300

    Inclusão de AbastractBO em Lei.

--- application/models/bo/Lei.php
+++ application/models/bo/Lei.php
@@ -1,78 +1,39 @@
 <?php
 
-defined('BASEPATH') or exit('No direct script access allowed');
+require_once 'AbstractBO.php';
 
-
-include_once('InterfaceBO.php');
-
-
-class Lei implements InterfaceBO
+class Lei extends AbstractBO
 {
-
-	private $id;
 	private $numero;
 	private $artigo;
 	private $inciso;
 	private $data;
 	private $modalidade;
-	private $status;
 
 	public function __construct(
 		$id,
+		$status,
 		$numero,
 		$artigo,
 		$inciso,
 		$data,
-		$modalidade,
-		$status = true
+		$modalidade
 	)
 	{
-		$this->id = $id;
+		parent::__construct($id, $status);
 		$this->numero = $numero;
 		$this->artigo = $artigo;
 		$this->inciso = $inciso;
-		$this->data = $data;
+		$this->data = $this->load->library('Data', $data);
 		$this->modalidade = $modalidade;
-		$this->status = $status;
-	}
-
-	function __get($key)
-	{
-		return $this->$key;
-	}
-
-	function __set($key, $value)
-	{
-		$this->$key = $value;
 	}
 
 	public function toString()
 	{
-
-		include_once 'application/libraries/Data.php';
-		$data = new Data($this->data);
-
-		$art = ($this->artigo != "") ? (", Art. " . $this->artigo) : '';
-		$inc = ($this->inciso != "") ? (", Inc. " . $this->inciso) : '';
-		$dat = ", de " . $data->formatoDoBrasil();
-
-		return $this->numero . $art . $inc . $dat;
+		return
+			'Art. ' . $this->artigo . '° ' .
+			empty($this->inciso) ? '' : ('inciso ' . $this->inciso . ', ') .
+				'Lei ' . $this->numero .
+				$this->data->formatoDoBrasil();
 	}
-
-	public function array()
-	{
-		include_once 'application/libraries/Data.php';
-		$data = new Data($this->data);
-
-		return array(
-			'id' => $this->id ?? null,
-			'numero' => $this->numero,
-			'artigo' => $this->artigo,
-			'inciso' => $this->inciso,
-			'data' => $data->formatoDoMySQL(),
-			'modalidade_id' => $this->modalidade->id,
-			'status' => $this->status
-		);
-	}
-
 }
--- application/models/dao/LeiDAO.php
+++ application/models/dao/LeiDAO.php
@@ -125,5 +125,19 @@ class LeiDAO extends CI_Model
 	{
 		return $this->buscarOnde('modalidade_id', $modalidadeId);
 	}
+	public function array()
+	{
+		include_once 'application/libraries/Data.php';
+		$data = new Data($this->data);
 
+		return array(
+			'id' => $this->id ?? null,
+			'numero' => $this->numero,
+			'artigo' => $this->artigo,
+			'inciso' => $this->inciso,
+			'data' => $data->formatoDoMySQL(),
+			'modalidade_id' => $this->modalidade->id,
+			'status' => $this->status
+		);
+	}
 }
