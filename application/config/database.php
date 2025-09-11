7648a747e4051997331fc5c2e762ccbcca61d007
Eliel de Souza Faria <elielrj@gmail.com>
2025-09-11 10:51:52 -0300

    Ajuste de correção do serviço db como hostname no docker

--- application/config/database.php
+++ application/config/database.php
@@ -76,7 +76,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
+	'hostname' => 'db',
 	'username' => 'root',
 	'password' => 'root',
 	'database' => 'processo_de_aquisicao',
